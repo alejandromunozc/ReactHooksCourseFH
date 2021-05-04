a97a887c2f06ed5b920271334fd201d090ff3e18
alejandromunozc <alejandromunozc@gmail.com>
2021-05-03 23:10:57 -0500

    useLayoutEffect, memo

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Memorize } from './components/06-memos/Memorize';
+//import { Layout } from './components/05-useLayoutEffect/Layout';
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { FocusScreen } from './components/04-useRef/FocusScreen';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
@@ -10,6 +12,6 @@ import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { HookApp } from './HookApp'
 
 ReactDOM.render(
-    <RealExampleRef />,
+    <Memorize />,
     document.getElementById('root')
 );
