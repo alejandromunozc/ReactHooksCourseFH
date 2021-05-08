3e57d4b7884b59521dfd72095fac67c3c9e94be4
alejandromunozc <alejandromunozc@gmail.com>
2021-05-07 20:43:55 -0500

    useContext section

--- src/components/09-useContext/LoginScreen.jsx
+++ src/components/09-useContext/LoginScreen.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {UserContext} from './UserContext';
 
 export const LoginScreen = () => {
+
+  const {setUser} = useContext(UserContext);
+
   return (
     <div>
       <h1>LoginScreen</h1>
       <hr />
+      <button onClick={ () => setUser({id:123, name: 'Cristian'})} className="btn btn-primary">Login</button>
     </div>
   )
 }
